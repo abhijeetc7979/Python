b33ac07a7aff15a85a988a3cda66d9a662379b4a
andyphan43 <harisankarmandal2090@gmail.com>
2022-08-28 21:46:58 +0530

    xyz

--- celcius_to_farenhit.py
+++ celcius_to_farenhit.py
@@ -1,17 +1,9 @@
 # fahrenheit = celsius * 1.8 + 32
-
-
 def ctof(c):
     return (c * 1.8) + 32
-
-
 c = int(input("temperature in Celsius:"))
 print(ctof(c))
-
-
 def ftoc(f):
     return (f - 32) / 1.8
-
-
 f = int(input("temperature in Fareinheight:"))
 print(ftoc(f))
