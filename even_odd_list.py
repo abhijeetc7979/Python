4b198eb39fd9d7202e8c9e70e3caf7fa787fe36c
andyphan43 <harisankarmandal2090@gmail.com>
2022-08-28 21:57:39 +0530

    coding standard added

--- even_odd_list.py
+++ even_odd_list.py
@@ -1,14 +1,14 @@
-num = int(input('Enter up to what number you want list of even numbers: - '))
+num = int(input("Enter up to what number you want list of even numbers: - "))
 even_list = []
 odd_list = []
 prime_list = []
-for i in range(1, num+1):
+for i in range(1, num + 1):
     if i % 2 == 0:
         even_list.append(i)
     else:
         odd_list.append(i)
 
-for i in range (2,num):
+for i in range(2, num):
     if i % 2:
         prime_list.append(i)
 
