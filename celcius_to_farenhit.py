edb42b873fd2e88e01fe4ba9b6f978bbab96df6f
andyphan43 <harisankarmandal2090@gmail.com>
2022-08-28 20:48:44 +0530

    first code

---
+++ celcius_to_farenhit.py
@@ -0,0 +1,13 @@
+#fahrenheit = celsius * 1.8 + 32
+
+def ctof(c):
+    return (c*1.8) + 32
+c= int(input('temperature in Celsius:'))
+print(ctof(c))
+
+
+# int(input('temperature in Celsius:'))
+#
+# f = (c * 1.8) + 32
+#
+# print(f)
